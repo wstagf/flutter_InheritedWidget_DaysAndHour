83e6e7cf87d966fbcc585ee6d6a7980566bbea73
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-03-25 14:23:42 -0300

    Adicionado data no titulo da coluna

--- lib/dayWidget.dart
+++ lib/dayWidget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:selectdayandhour/hourWidget.dart';
+import 'package:intl/intl.dart';
 
 class DayWidget extends StatelessWidget {
   final int position;
@@ -12,12 +13,19 @@ class DayWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: position.isEven ? Colors.grey[300] : Colors.white,
-      padding: EdgeInsets.all(20),
       height: MediaQuery.of(context).size.height,
       child: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
+            Container(
+              width: 130,
+              height: 60,
+              color: Colors.blue[200],
+              child: Center(
+                child: Text(DateFormat.MMMEd('pt_BR').format(day)),
+              ),
+            ),
             HourWidget(label: "06:00", active: false),
             HourWidget(label: "08:00", active: false),
             HourWidget(label: "09:00", active: false),
